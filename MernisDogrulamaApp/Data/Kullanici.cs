6ed625229b6f98ab681e666f8fd29f7292625cdd
Mert Metin <mertmetin1993@gmail.com>
2021-05-15 18:11:02 +0300

    Initial Commit

---
+++ MernisDogrulamaApp/Data/Kullanici.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MernisDogrulamaApp.Data
+{
+    public class Kullanici
+    {
+        public string Ad { get; set; }
+
+        public string Soyad { get; set; }
+
+        public string TcKimlikNo { get; set; }
+
+        public DateTime DogumTarihi { get; set; }
+    }
+}
---
+++ MernisDogrulamaApp/Data/MernisService.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+using System.Threading.Tasks;
+using static Mernis.KPSPublicSoapClient;
+
+namespace MernisDogrulamaApp.Data
+{
+    public class MernisService
+    {
+        public Task<bool> TcKimlikDogrula(Kullanici kullanici)
+        {
+            bool dogrulamaSonucu = false;
+            try
+            {
+                var mernisClient = new Mernis.KPSPublicSoapClient(EndpointConfiguration.KPSPublicSoap);
+                var tcKimlikDogrulamaServisResponse = mernisClient.TCKimlikNoDogrulaAsync(long.Parse(kullanici.TcKimlikNo), kullanici.Ad, kullanici.Soyad, kullanici.DogumTarihi.Year).GetAwaiter().GetResult();
+                dogrulamaSonucu = tcKimlikDogrulamaServisResponse.Body.TCKimlikNoDogrulaResult;
+            }
+            catch (Exception ex)
+            {
+                dogrulamaSonucu = false;
+            }
+            return Task.FromResult(dogrulamaSonucu);
+        }
+    }
+}
